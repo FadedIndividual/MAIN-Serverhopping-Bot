8c88e7f13050cc4564ee0202658ba8aae8daf318
FadedIndividual <141731826+FadedIndividual@users.noreply.github.com>
2024-06-17 08:03:34 -0700

    Update STRINGS.lua

--- STRINGS.lua
+++ STRINGS.lua
@@ -8,10 +8,6 @@ local Table={
 		"I'm very nice don't worry! I'm very nice don't worry! I'm very nice don't worry! I'm very nice don't worry! I'm very nice don't worry!"
 	},
 	{
-		"Welcome back! I'd love to introduce myself. I am a Fluent LUA, LUAU Coder & I'm learning PYTHON Aswell",
-		"I've came back to the BOTTING SCENE because I wanted to make something better than ever, I'm using an EMULATOR!"
-	},
-	{
 		"Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41",
 		"Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41"
 	},

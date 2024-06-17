c0f7f67d15acad9f5cb38c2f6364928f1e4ce0d7
FadedIndividual <141731826+FadedIndividual@users.noreply.github.com>
2024-06-17 05:33:57 -0700

    Create STRINGS.lua

---
+++ STRINGS.lua
@@ -0,0 +1,27 @@
+local Table={
+	{
+		"I own you I own you I own you I own you I own you I own you I own you I own you I own you I own you I own you I own you",
+		"YOU CANT BE BETTER YOU CANT BE BETTER YOU CANT BE BETTER YOU CANT BE BETTER YOU CANT BE BETTER YOU CANT BE BETTER YOU CANT BE BETTER"
+	},
+	{
+		"Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41",
+		"I'm very nice don't worry! I'm very nice don't worry! I'm very nice don't worry! I'm very nice don't worry! I'm very nice don't worry!"
+	},
+	{
+		"Welcome back! I'd love to introduce myself. I am a Fluent LUA, LUAU Coder & I'm learning PYTHON Aswell",
+		"I've came back to the BOTTING SCENE because I wanted to make something better than ever, I'm using an EMULATOR!"
+	},
+	{
+		"Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41",
+		"Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41"
+	},
+	{
+		"EZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZ",
+		"EZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZ",
+	},
+	{
+		"Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41",
+		"Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41"
+	}
+}
+return Table

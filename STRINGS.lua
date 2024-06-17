05333b64c321f64541a02518b7e4b8d5ef826097
FadedIndividual <141731826+FadedIndividual@users.noreply.github.com>
2024-06-17 14:04:30 -0700

    Update STRINGS.lua

--- STRINGS.lua
+++ STRINGS.lua
@@ -1,27 +1,7 @@
 local Table={
 	{
-		"I own you I own you I own you I own you I own you I own you I own you I own you I own you I own you I own you I own you",
-		"YOU CANT BE BETTER YOU CANT BE BETTER YOU CANT BE BETTER YOU CANT BE BETTER YOU CANT BE BETTER YOU CANT BE BETTER YOU CANT BE BETTER"
-	},
-	{
-		"Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41",
-		"I'm very nice don't worry! I'm very nice don't worry! I'm very nice don't worry! I'm very nice don't worry! I'm very nice don't worry!"
-	},
-	{
 		"Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41",
 		"Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41"
-	},
-	{
-		"EZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZ",
-		"EZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZEZ",
-	},
-	{
-		"Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41",
-		"Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41 | Add on -BLUE APP- @ready41"
-	},
-	{
-		"BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP",
-		"BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP BEEP"
 	}
 }
 return Table

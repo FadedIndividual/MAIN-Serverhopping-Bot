597d75cf3005a613ed99eade679d93393b3a7364
FadedIndividual <141731826+FadedIndividual@users.noreply.github.com>
2024-06-18 03:26:25 -0700

    Update jaded.lua

--- jaded.lua
+++ jaded.lua
@@ -31,6 +31,7 @@ end
 Funk = function(pqs, Original)
 	if pqs and pqs.Character and pqs.Character.Humanoid and pqs.Character.Humanoid.Health >= 1.5 then
 		local num = 0
+		local rndd = math.random(1, 5)
 		local timeee = tick()
 		repeat task.wait()
 			pcall(function()
@@ -38,10 +39,10 @@ Funk = function(pqs, Original)
 				local Ours, Theirs = lp.Character:FindFirstChild("HumanoidRootPart"), pqs.Character:FindFirstChild("HumanoidRootPart")
 				
 				if Ours and Theirs then
-					spawn(Funk_RV) Ours.CFrame = Theirs.CFrame * CFrame.Angles(0, math.rad(num), 0) * CFrame.new(0, 0, 2) spawn(Funk_RV)
+					spawn(Funk_RV) Ours.CFrame = Theirs.CFrame * CFrame.Angles(0, math.rad(num), 0) * CFrame.new(0, 0, rndd) spawn(Funk_RV)
 				end
 			end)
-		until not pqs or not pqs.Character or pqs.Character:FindFirstChild("Humanoid").Health <= 1.5 or not pqs.Character:FindFirstChild("HumanoidRootPart") or (pqs.Character:FindFirstChild("HumanoidRootPart").Position - Original).Magnitude >= 7 or tick() - timeee >= 15
+		until not pqs or not pqs.Character or pqs.Character:FindFirstChild("Humanoid").Health <= 1.5 or not pqs.Character:FindFirstChild("HumanoidRootPart") or (pqs.Character:FindFirstChild("HumanoidRootPart").Position - Original).Magnitude >= 13 or tick() - timeee >= 15
 		return
 	else
 		return

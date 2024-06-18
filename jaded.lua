8cecd7535b88e42d8406e3ca0a435a0c9f2dc646
FadedIndividual <141731826+FadedIndividual@users.noreply.github.com>
2024-06-18 11:48:49 -0700

    Update jaded.lua

--- jaded.lua
+++ jaded.lua
@@ -61,7 +61,7 @@ Funk = function(pqs, Original)
 							Anim:Play()
 							Anim:AdjustSpeed(10)
 						end)
-						spawn(Funk_RV) Ours.CFrame = Theirs.CFrame * CFrame.Angles(0, math.rad(0), 0) * CFrame.new(0, 2, .85) spawn(Funk_RV)
+						spawn(Funk_RV) Ours.CFrame = Theirs.CFrame * CFrame.Angles(0, math.rad(0), 0) * CFrame.new(0, -1, .85) spawn(Funk_RV)
 					end
 				end
 			end)

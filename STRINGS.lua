e6c6fd8b3c21a5f08e362afe12f140af5d1b7a2d
FadedIndividual <141731826+FadedIndividual@users.noreply.github.com>
2024-06-18 23:19:45 -0700

    Update STRINGS.lua

--- STRINGS.lua
+++ STRINGS.lua
@@ -5,24 +5,16 @@ local Table={
 		"-- -- -- -- -- -- -- - made this -- -- -- -- -- -- --"
 	},
 	{
-		"@ready41",
-		"@ready41"
-	},
-	{
 		"Would you like to Advertise something Or say something silly? I would love for you to add me & I'll include whatever you want!",
 		"@ ready41"
-	},
-	{
-		"you're weird",
-		"LOOOOOOOOOOL"
 	},
 	{
 		"-|BLUE|- -|Ghost|- -|App|-",
 		"- - @ready41 - -"
 	},
 	{
-		"🤡 you are a clown 🤡",
-		"@ ready41"
+		"[SALE] ROBUX $4/1K ADD NETSPEND123 ON DC",
+		"[SALE] ROBUX $4/1K ADD NETSPEND123 ON DC"
 	}
 }
 return Table

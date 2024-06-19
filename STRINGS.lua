0964ec020bea345be567cfbb23fc7c7ba493a973
FadedIndividual <141731826+FadedIndividual@users.noreply.github.com>
2024-06-19 08:39:49 -0700

    Update STRINGS.lua

--- STRINGS.lua
+++ STRINGS.lua
@@ -11,6 +11,10 @@ local Table={
 	{
 		"-|BLUE|- -|Ghost|- -|App|-",
 		"- - @ready41 - -"
+	},
+	{
+		"Would you like to Advertise something Or say something silly? I would love for you to add me & I'll include whatever you want!",
+		"@ ready41"
 	}
 }
 return Table

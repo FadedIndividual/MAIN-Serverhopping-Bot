177adea3c235b2650fe8fa1435bd63b2f85f95f5
FadedIndividual <141731826+FadedIndividual@users.noreply.github.com>
2024-06-19 02:30:10 -0700

    Update STRINGS.lua

--- STRINGS.lua
+++ STRINGS.lua
@@ -11,10 +11,6 @@ local Table={
 	{
 		"-|BLUE|- -|Ghost|- -|App|-",
 		"- - @ready41 - -"
-	},
-	{
-		"[SALE] ROBUX $4/1K ADD NETSPEND123 ON DC",
-		"[SALE] ROBUX $4/1K ADD NETSPEND123 ON DC"
 	}
 }
 return Table

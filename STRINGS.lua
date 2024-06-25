9d64858aaeea9132bc66ef621cf375c68c53c59a
FadedIndividual <141731826+FadedIndividual@users.noreply.github.com>
2024-06-25 07:59:02 -0700

    Update STRINGS.lua

--- STRINGS.lua
+++ STRINGS.lua
@@ -25,8 +25,8 @@ local Table={
 		"@fattii2 to join freak👅 dizhurd /PNtFRfQt"
 	},
 	{
-		"https://www.roblox.com/groups/7639105/Devils-For-Glory#!/about check it out for actual good clothes,
-		"https://www.roblox.com/groups/7639105/Devils-For-Glory#!/about check it out for actual good clothes"
+		"https://www.roblox.com/groups/7639105/Devils-For-Glory! check it out for actual good clothes,
+		"https://www.roblox.com/groups/7639105/Devils-For-Glory! check it out for actual good clothes"
 	}
 }
 return Table

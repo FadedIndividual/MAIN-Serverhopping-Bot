678facf05ba119e0afa6ff2f96dedb3f1a4d5c92
FadedIndividual <141731826+FadedIndividual@users.noreply.github.com>
2024-06-25 09:28:33 -0700

    Update STRINGS.lua

--- STRINGS.lua
+++ STRINGS.lua
@@ -2,15 +2,15 @@ setfpscap(20)
 local Table={
 	{
 		"Would you like to run an AD (a msg on the bot) Or say something silly?",
-		"@ ready41"
+		"@ready41 "
 	},
 	{
 		"Would you like something to say Or just get a msg out there?",
-		"@ ready41"
+		"@ready41 "
 	},
 	{
 		"I'll include whatever you want to say!",
-		"@ ready41"
+		"@ready41 "
 	},
 	{
 		"@fattii2 to join freak👅 dizhurd /PNtFRfQt",
